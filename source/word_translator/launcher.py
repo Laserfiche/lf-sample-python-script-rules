f6d07a321ed83c0c4f6088f039116ef9c9c5f43d
Kern.Ding <kern.ding@laserfiche.com>
2023-10-05 17:32:04 -0700

    add launcher.py

---
+++ source/word_translator/launcher.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import sys
+sys.path.append('../..')  # add root to PythonPath
+
+from utility.lf_arg_parser.src.lf_arg_parser import get_argument_map
+from src.translator import translate
+
+if __name__ == "__main__":
+    # Inputs can be automatically parsed with the provided function
+    # Or parsed manually via 'sys.argv'
+    inputsMap = get_argument_map(sys.argv)
+
+    sentence = inputsMap['Sentence']
+    language = inputsMap["LanguageCode"]
+    translated = translate(sentence, language)
+
+    # Output can be printed to the console in `key:value` pairs format
+    # remote agent will parse the output and return it to the caller
+    print(f"translated : {translated}")
--- source/word_translator/src/translator.py
+++ source/word_translator/src/translator.py
@@ -4,10 +4,12 @@ from translate import Translator
 def translate(sentence: str, lang: str) -> str:
     """
     Translates a sentence into the specified language.
+    Uses 'translate module'.  See https://pypi.org/project/translate/ for more information.
 
     :param sentence: The sentence to translate.
     :type sentence: str
     :param lang: The language to translate the sentence into.
+                 See https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes for valid language codes.
     :type lang: str
     :return: The translated sentence.
     :rtype: str
