d4e30f7eb66c8afd52dfe1be814fcbc830579d11
Kern.Ding <kern.ding@laserfiche.com>
2023-10-16 09:45:00 -0700

    more comments

--- utility/lf_arg_parser/src/lf_arg_parser.py
+++ utility/lf_arg_parser/src/lf_arg_parser.py
@@ -2,9 +2,19 @@
 # Licensed under the MIT License. See LICENSE in the project root for license information.
 
 import sys
+from typing import List, Dict
 
 
-def get_argument_map(argument_list):
+def get_argument_map(argument_list: List[str]) -> Dict[str, str]:
+    """
+    Parses the argument list and returns a dictionary of key-value pairs.
+
+    :param argument_list: A list of command-line arguments.
+    :type argument_list: list[str]
+    :return: A dictionary of key-value pairs representing the parsed arguments.
+    :rtype: dict[str, str]
+    :raises Exception: If the argument list is invalid.
+    """
     argument_map = {}
     if argument_list is None:
         raise Exception("Invalid arguments.")
